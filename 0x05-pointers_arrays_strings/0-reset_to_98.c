8572b2ac8dbf078d6c23daf5428fe1551624e7a8
reucod <reucod@gmial.com>
2023-04-01 13:44:37 -0700

    0x05-pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function that takes a pointer to an int as parameter and update
+ * reset_to_98 - function that takes a pointer
  * @n:- the value to be updated to 98
  * Return: Always 0
  */
