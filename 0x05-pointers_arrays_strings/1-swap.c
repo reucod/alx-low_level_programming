4575ec6f5e8544ad031ab272c17f06881565699e
reucod <reucod@gmial.com>
2023-04-01 13:58:40 -0700

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - take in two variable integer
+ * @author Anisah
+ * @a: swaps and stores address of b
+ * @b: swaps and stores address of a
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+int change;
+change = *b;
+*b = *a;
+*a = change;
+}
